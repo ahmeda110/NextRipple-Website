8c3571b52c9309f796b384c1fc8cef2468e6a50c
bartek717 <bartekkowalski925@gmail.com>
2024-02-05 18:13:42 -0500

    stash layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,9 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
 
-
-export const Home = () => {
+function Home() {
   return (
-    <Container>
-        <Row>
-            <Col>
-            <div className='home-intro'>
-            <h2>
-            Next Ripple
-            </h2>
-            <h1>
-                Elevating Lives
-            </h1>
-            <p style={{color:'grey'}}>
-                Your gateway to Mental Resilience, Physical Wellness, and Global Exploration. The ripple of change starts with you.
-            </p>
-            </div>
-            
-                </Col>
-            <Col>HOme</Col>
-        </Row>
-    </Container>
+    <div>Home</div>
   )
 }
+
+export default Home
