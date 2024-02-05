61a44c1cb57def626a6224a6e3cfd63efe8442a2
Kristen Zhang <intentuskristen@gmail.com>
2024-02-05 13:19:45 -0500

    home page title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,20 @@ export const Home = () => {
   return (
     <Container>
         <Row>
-            <Col>HOme</Col>
+            <Col>
+            <div className='home-intro'>
+            <h2>
+            Next Ripple
+            </h2>
+            <h1>
+                Elevating Lives
+            </h1>
+            <p style={{color:'grey'}}>
+                Your gateway to Mental Resilience, Physical Wellness, and Global Exploration. The ripple of change starts with you.
+            </p>
+            </div>
+            
+                </Col>
             <Col>HOme</Col>
         </Row>
     </Container>
