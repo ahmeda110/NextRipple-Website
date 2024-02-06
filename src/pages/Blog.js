765f97f01841d43148880d96bca610d102ba2459
bartek717 <bartekkowalski925@gmail.com>
2024-02-05 21:41:26 -0500

    basic blog styling

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,27 +1,40 @@
-import React from 'react'
-import styles from './Blog.module.css'
-import { blogArticles } from '../components/BlogData'
-
+import React from 'react';
+import styles from './Blog.module.css';
+import { blogArticles } from '../components/BlogData';
 
 function Blog() {
-    return (
-      <div className={styles.blogContainer}>
-          <div className={styles.blogHeader}>
-              <h2>Most Recent Blogs</h2>
-              <button className={styles.viewAllButton} onClick={() => {}}>View All Blogs</button>
-          </div>
-          <div className={styles.articlesContainer}>
-              {blogArticles.map((article, index) => (
-                  <div key={index} className={styles.article}>
-                      <img className={styles.articlePhoto} src={article.image} alt={"Article Image"}/>
-                      <div className={styles.articleCategory}>{article.category}</div>
-                      <h3 className={styles.articleTitle}>{article.title}</h3>
-                      <p className={styles.articleDescription}>{article.description}</p>
-                  </div>
-              ))}
-          </div>
-      </div>
-    );
-  }
-  
-  export default Blog;
+  const firstArticle = blogArticles.slice(0, 1);
+  const nextTwoArticles = blogArticles.slice(1, 3);
+
+  return (
+    <div className={styles.blogContainer}>
+        <div className={styles.blogHeader}>
+            <h2>Most Recent Blogs</h2>
+            <button className={styles.viewAllButton} onClick={() => {/* Implement navigation logic */}}>View All Blogs</button>
+        </div>
+        <div className={styles.articlesWrapper}>
+            <div className={styles.mainArticle}>
+                {firstArticle.map((article, index) => (
+                    <div key={index} className={styles.article}>
+                        <img className={styles.articlePhoto} src={article.image} alt="Article Image"/>
+                        <div className={styles.articleCategory}>{article.category}</div>
+                        <h3 className={styles.articleTitle}>{article.title}</h3>
+                        <p className={styles.articleDescription}>{article.description}</p>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.subArticle}>
+                {nextTwoArticles.map((article, index) => (
+                    <div key={index} className={styles.article}>
+                        <img className={styles.articlePhoto} src={article.image} alt="Article Image"/>
+                        <div className={styles.articleCategory}>{article.category}</div>
+                        <h3 className={styles.articleTitle}>{article.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Blog;
