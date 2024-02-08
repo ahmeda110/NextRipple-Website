3738728f2b8e9a3517576b5085753575e6369781
Kristen Zhang <intentuskristen@gmail.com>
2024-02-08 16:02:42 -0500

    added section 2 of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 export const Home = () => {
   return (
-    <Container>
+    <div>
+        <Container>
         <Row>
             <Col>
             <div className='home-intro'>
@@ -20,8 +21,21 @@ export const Home = () => {
             </div>
             
                 </Col>
-            <Col>HOme</Col>
+            <Col>
+            <img src='./img/NR_rocket.png' alt='rocket image' className='rocket'></img>
+            </Col>
         </Row>
     </Container>
+    <Container fluid className='home-2'>
+    <Row>
+        <Col style={{paddingLeft: '5%'}}>
+        <img src='./img/home-2.png' alt='home-2' className='section-2'></img>
+        </Col>
+        <Col className='text-2'>
+        <h1 style={{color: 'white'}}>Find The <span style={{color: '#FFAE71'}}>Right Path</span> With Us</h1>
+        </Col>
+    </Row>
+    </Container>
+    </div>
   )
 }
