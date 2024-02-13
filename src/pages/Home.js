2175e0b57b7922672b2acfb250fa2b2b454c7533
Kristen Zhang <intentuskristen@gmail.com>
2024-02-13 10:10:17 -0500

    added universal button styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,57 +1,168 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 export const Home = () => {
   return (
     <div>
-        <Container>
+      <Container>
         <Row>
-            <Col>
-            <div className='home-intro'>
-            <h2>
-            Next Ripple
-            </h2>
-            <h1>
-                Elevating Lives
-            </h1>
-            <p style={{color:'grey'}}>
-                Your gateway to Mental Resilience, Physical Wellness, and Global Exploration. The ripple of change starts with you.
-            </p>
+          <Col>
+            <div className="home-intro">
+              <h2>Next Ripple</h2>
+              <h1>Elevating Lives</h1>
+              <p style={{ color: "grey" }}>
+                Your gateway to Mental Resilience, Physical Wellness, and Global
+                Exploration. The ripple of change starts with you.
+              </p>
+              <button className="button1">Our Products</button>
             </div>
-            
-                </Col>
-            <Col>
-            <img src='./img/NR_rocket.png' alt='rocket image' className='rocket'></img>
-            </Col>
+          </Col>
+          <Col>
+            <img
+              src="./img/NR_rocket.png"
+              alt="rocket image"
+              className="rocket"
+            ></img>
+          </Col>
         </Row>
-    </Container>
-    <Container fluid className='home-2'>
-    <Row>
-        <Col style={{paddingLeft: '5%'}}>
-        <img src='./img/home-2.png' alt='home-2' className='section-2'></img>
-        </Col>
-        <Col className='text-2'>
-        <h1 style={{color: 'white'}}>Find The <span style={{color: '#FFAE71'}}>Right Path</span> With Us</h1>
-        <div className="header-container" style={{ position: 'relative' }}>
-  <img src='./img/mental_icon.png' alt='icon' className='icon' />
-  <h1 style={{ color: 'white', fontSize: '1em', position: 'absolute', left: '30%', top: '40%', transform: 'translate(-50%, -50%)', zIndex: '1', margin: '0' }}>Mental Resilience</h1>
-  <img src='./img/divider.png' alt='divider' className='divider' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '0' }} />
-</div>
-        <div className="header-container" style={{ position: 'relative' }}>
-  <img src='./img/mental_icon.png' alt='icon' className='icon' />
-  <h1 style={{ color: 'white', fontSize: '1em', position: 'absolute', left: '30%', top: '40%', transform: 'translate(-50%, -50%)', zIndex: '1', margin: '0' }}>Mental Resilience</h1>
-  <img src='./img/divider.png' alt='divider' className='divider' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '0' }} />
-</div>
-<div className="header-container" style={{ position: 'relative' }}>
-  <img src='./img/mental_icon.png' alt='icon' className='icon' />
-  <h1 style={{ color: 'white', fontSize: '1em', position: 'absolute', left: '30%', top: '40%', transform: 'translate(-50%, -50%)', zIndex: '1', margin: '0' }}>Mental Resilience</h1>
-  <img src='./img/divider.png' alt='divider' className='divider' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '0' }} />
-</div>
-        </Col>
-
-    </Row>
-    </Container>
+      </Container>
+      <Container fluid className="home-2">
+        <Row>
+          <Col style={{ paddingLeft: "5%" }}>
+            <img
+              src="./img/home-2.png"
+              alt="home-2"
+              className="section-2"
+            ></img>
+          </Col>
+          <Col className="text-2">
+            <h1 style={{ color: "white" }}>
+              Find The <span style={{ color: "#FFAE71" }}>Right Path</span> With
+              Us
+            </h1>
+            <div className="header-container" style={{ position: "relative" }}>
+              <img src="./img/mental_icon.png" alt="icon" className="icon" />
+              <h1
+                style={{
+                  color: "white",
+                  fontSize: "1em",
+                  position: "absolute",
+                  left: "30%",
+                  top: "40%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "1",
+                  margin: "0",
+                }}
+              >
+                Mental Resilience
+              </h1>
+              <img
+                src="./img/divider.png"
+                alt="divider"
+                className="divider"
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "0",
+                }}
+              />
+              <p
+                style={{
+                  position: "absolute",
+                  left: "72px",
+                  top: "50%",
+                  zIndex: "1",
+                  margin: "0",
+                }}
+              >
+                Lorem ipsum jalksd;fjlakdsjfl;kdsjfkl;dsadsfdfsjf;slkjdf ;
+              </p>
+            </div>
+            <div className="header-container" style={{ position: "relative" }}>
+              <img src="./img/physical_icon.png" alt="icon" className="icon" />
+              <h1
+                style={{
+                  color: "white",
+                  fontSize: "1em",
+                  position: "absolute",
+                  left: "30%",
+                  top: "40%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "1",
+                  margin: "0",
+                }}
+              >
+                Physical Wellness
+              </h1>
+              <img
+                src="./img/divider.png"
+                alt="divider"
+                className="divider"
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "0",
+                }}
+              />
+              <p
+                style={{
+                  position: "absolute",
+                  left: "72px",
+                  top: "50%",
+                  zIndex: "1",
+                  margin: "0",
+                }}
+              >
+                Lorem ipsum jalksd;fjlakdsjfl;kdsjfkl;dsadsfdfsjf;slkjdf ;
+              </p>
+            </div>
+            <div className="header-container" style={{ position: "relative" }}>
+              <img src="./img/global_icon.png" alt="icon" className="icon" />
+              <h1
+                style={{
+                  color: "white",
+                  fontSize: "1em",
+                  position: "absolute",
+                  left: "30%",
+                  top: "40%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "1",
+                  margin: "0",
+                }}
+              >
+                Global Exploration
+              </h1>
+              <img
+                src="./img/divider.png"
+                alt="divider"
+                className="divider"
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "0",
+                }}
+              />
+              <p
+                style={{
+                  position: "absolute",
+                  left: "72px",
+                  top: "50%",
+                  zIndex: "1",
+                  margin: "0",
+                }}
+              >
+                Lorem ipsum jalksd;fjlakdsjfl;kdsjfkl;dsadsfdfsjf;slkjdf ;
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
