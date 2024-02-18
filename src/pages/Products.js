f396116910bc0a0f7becb4e218cd1e4a2cfbf92a
Stanley-Cao <stanleycao23@gmail.com>
2024-02-17 21:32:13 -0500

    Added the styling for description and images

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import styles from './Products.module.css';
 import PageHeader from '../components/PageHeader';
-import fitness from '../assets/fitness-app-image.png'
+import fitness from '../assets/fitness-app-image.png';
+import itinerary from '../assets/itinerary-app-image.png'; 
 
 function Products() {
   const products = [
     {
       title: 'Fitness App',
       description: 'Transform your fitness journey with our new Fitness App! Personalized training plans tailored to your needs and goals. Stay motivated, improve your health with efficiency.',
-      image: '../assets/fitness-app-image.png',
+      image: fitness,
     },
     {
       title: 'Itinerary App',
       description: 'Discover the ultimate travel companion in our Itinerary App! Personalized itinerary plans ensuring you have memorable & planning-free traveling experiences.',
-      image: '../assets/fitness-app-image.png', 
+      image: itinerary, 
     } 
   ]; 
 
@@ -21,10 +22,11 @@ function Products() {
     <main>
       <PageHeader title="Our Products" />
       <div className={styles.Products}>
+        <h1>Our Products</h1>
         {products.map((product, index) => (
-          <div key={index} className={styles.productContainer}>
+          <div key={index} className={`${styles.productContainer} ${product.title === 'Itinerary App' ? styles.itineraryLayout : ''}`}>
             <div className={styles.imageContainer}>
-              <img src={fitness} alt={product.title} className={styles.productImage} />
+              <img src={product.image} alt={product.title} className={styles.productImage} />
             </div>
             <div className={styles.productDetails}>
               <h2>{product.title}</h2>
